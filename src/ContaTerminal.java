e99d03d92d3b541b3566209d430b0bc5809c4e54
Timafejn, Yuri <timafejn@gmail.com>
2023-08-01 08:15:50 -0300

    Feitas as funcionalidades solicitadas no projeto

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,11 +1,27 @@
+import cliente.Cliente;
+
 import java.io.InputStream;
 import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
-        
+        Cliente cliente = new Cliente();
 
+        System.out.println("Digite o número da conta: ");
+        cliente.setNumero(scanner.nextInt());
+        scanner.nextLine();
+
+        System.out.println("Digite o número da agência: ");
+        cliente.setAgencia(scanner.nextLine());
+
+        System.out.println("Digite o nome do cliente: ");
+        cliente.setNomeCliente(scanner.nextLine());
+
+        System.out.println("Digite o saldo do sr(a). " + cliente.getNomeCliente() + ": ");
+        cliente.setSaldo(scanner.nextDouble());
         scanner.close();
+
+        System.out.println(cliente);
     }
 }
